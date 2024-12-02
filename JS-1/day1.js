ca9e03ad1f7dba4dd848a315949f98457846fa52
Divya <divya.reddy79@gmail.com>
2024-12-02 15:56:58 +0000

    firstCommit

---
+++ JS-1/day1.js
@@ -0,0 +1,9 @@
+console.log("HI!!!!!!!!!")
+
+const balance = 100.50
+console.log(typeof balance);
+
+let deposit = prompt('Deposit Amount')
+
+console.log('your total balance is ', +deposit + balance)
+
